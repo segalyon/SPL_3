73da3fe990d54482665b3926f78ff78e078d8881
segalyon <markpotchli@gmail.com>
2022-01-06 18:19:42 +0200

    finish

--- Server/src/main/java/bgu/spl/net/api/bidi/BidiMessagingProtocol.java
+++ Server/src/main/java/bgu/spl/net/api/bidi/BidiMessagingProtocol.java
@@ -3,12 +3,11 @@ package bgu.spl.net.api.bidi;
 import bgu.spl.net.api.Messages.Message;
 import bgu.spl.net.api.MessagingProtocol;
 
-public interface BidiMessagingProtocol<T> extends MessagingProtocol<T> {
+public interface BidiMessagingProtocol<T>  {
     /**
      * Used to initiate the current client protocol with it's personal connection ID and the connections implementation
      **/
     void start(int connectionId, Connections<T> connections);
-
     void process(T message);
-
+    boolean shouldTerminate();
 }
